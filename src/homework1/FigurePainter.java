8a420657bf8b394c84c45447c59bbd4ea12e18cf
User <zakaryanarmine991@gmail.com>
2023-08-31 18:41:30 +0400

    files

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -20,8 +20,48 @@ public class FigurePainter {
             }
             System.out.println();
             System.out.println();
-
-
         }
-    }
-}
+        for (int i = 5; i > 0; i--) {
+            for (int k = 0; k < i - 1; k++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= (5 - i); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            for (int k = 1; k <=i ; k++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= (4 - i); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 5; i > 0; i--) {
+            for (int k = 0; k < i - 1; k++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= (5 - i); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int k = 0; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= (3 - i); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+            }
+        }
