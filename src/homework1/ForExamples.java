b5d5c398bc7fa8b0c46ecb4b27fed245eae508f5
User <zakaryanarmine991@gmail.com>
2023-09-02 21:08:47 +0400

    files

---
+++ src/homework1/ForExamples.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class ForExamples {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 1;  i<= 1000; i++) {
+            System.out.print("-");
+            System.out.print(i);
+
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int k = 1; k<= 100;k ++){
+            if (k%2==0){
+                System.out.println(k );
+            }
+        }
+    }
+    }
