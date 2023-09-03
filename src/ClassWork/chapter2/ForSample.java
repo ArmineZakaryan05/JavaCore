91c14550fc381d2f08a8f6d11b31673ce240dac1
User <zakaryanarmine991@gmail.com>
2023-09-03 20:43:56 +0400

    files

--- src/ClassWork/chapter2/ForSample.java
+++ src/ClassWork/chapter2/ForSample.java
@@ -3,7 +3,7 @@ package ClassWork.chapter2;
 public class ForSample {
     public static void main(String[] args) {
         System.out.println("******");
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("******");
 
         }
--- src/homework1/ForExamples.java
+++ src/homework1/ForExamples.java
@@ -2,19 +2,30 @@ package homework1;
 
 public class ForExamples {
     public static void main(String[] args) {
-        int i = 0;
-        for (i = 1;  i<= 1000; i++) {
-            System.out.print("-");
+        int i =0;
+        for ( i = 1; i <= 1000; i++) {
+            System.out.print(" - ");
             System.out.print(i);
-
         }
+            System.out.println();
+            System.out.println();
+
+            for (int k = 1; k <= 100; k++) {
+                if (k % 2 == 0) {
+                    System.out.println(k);
+                }
+            }
         System.out.println();
         System.out.println();
 
-        for (int k = 1; k<= 100;k ++){
-            if (k%2==0){
-                System.out.println(k );
+        int[] array = {2,5,8,4,9,3,7};
+
+        int max = array [0];
+        for (int j=0;j<array.length;j++){
+            if (array[j] > max){
+                max = array[j];
             }
         }
-    }
+        System.out.println("max: " + max);
+        }
     }
