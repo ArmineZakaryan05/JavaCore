dce61bfdc5455dde5605876d71b28898175aa2d8
User <zakaryanarmine991@gmail.com>
2023-09-12 13:59:50 +0400

    files

--- src/homework1/Sort.java
+++ src/homework1/Sort.java
@@ -2,12 +2,10 @@ package homework1;
 
 public class Sort {
     public static void main(String[] args) {
-
         int[] array = {4, 7, 1, 3, 9, 0, 2};
-
         int a;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
+            for (int j = i +1 ; j < array.length; j++) {
                 if (array[i] > array[j]) {
                     a = array[i];
                     array[i] = array[j];
