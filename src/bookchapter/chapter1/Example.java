bb74582b2a159d6ddb14ad4872ef12e8ce095a2c
User <zakaryanarmine991@gmail.com>
2024-07-07 22:27:15 +0400

    Example

--- src/bookchapter/chapter1/Example.java
+++ src/bookchapter/chapter1/Example.java
@@ -8,10 +8,5 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a simple Java program.");
-        System.out.println();
-        System.out.println("Բարև ես քեզի կարոդնամգը նույնիսկ քու կոխքիդ...");
-        System.out.println();
-        System.out.println();
-        System.out.println("Լույս կա քո ներսում հոգիս է մերսում...");
     }
 }
