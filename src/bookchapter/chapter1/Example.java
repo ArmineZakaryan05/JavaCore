57029e3c01cf25d8003e60a0475fe12b51870edf
User <zakaryanarmine991@gmail.com>
2024-07-07 21:47:51 +0400

    Example

---
+++ src/bookchapter/chapter1/Example.java
@@ -0,0 +1,17 @@
+package bookchapter.chapter1;
+/*
+This is a simple Java program.
+Call this file "Example.java".
+ */
+public class Example {
+    //Your program begins with a call to main().
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println();
+        System.out.println("Բարև ես քեզի կարոդնամգը նույնիսկ քու կոխքիդ...");
+        System.out.println();
+        System.out.println();
+        System.out.println("Լույս կա քո ներսում հոգիս է մերսում...");
+    }
+}
