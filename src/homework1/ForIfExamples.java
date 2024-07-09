5806fa407736aa263a28d8871715ba6cee55ef24
User <zakaryanarmine991@gmail.com>
2024-07-09 21:19:59 +0400

    Example

---
+++ src/homework1/ForIfExamples.java
@@ -0,0 +1,45 @@
+package homework1;
+
+public class ForIfExamples {
+    public static void main(String[] args) {
+        int x;
+        for (x = 1; x < 11; x++) {
+            System.out.print(" " + x);
+        }
+        System.out.println();
+        System.out.println();
+
+
+        int sum = 0;
+        for (int i = 1; i < 101; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+        System.out.println();
+        System.out.println();
+
+
+        int y;
+        for (y = 50; y > 0; y--) {
+            System.out.print(" " + y);
+        }
+        System.out.println();
+        System.out.println();
+
+
+        int z;
+        for (z = 1; z < 20; z = z + 2) {
+            System.out.print(" " + z);
+        }
+        System.out.println();
+        System.out.println();
+
+
+        int count = 0;
+        for (int a = 2; a < 100; a = a + 2) {
+            count = count + 1;
+        }
+        System.out.println(" " + count);
+
+    }
+}
