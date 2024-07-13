1e12ebe7be7326a4d1c88ad5bf28085f9b2efa5d
User <zakaryanarmine991@gmail.com>
2024-07-13 12:45:44 +0400

    Example

---
+++ src/bookchapter/chapter3/Adg.java
@@ -0,0 +1,8 @@
+package bookchapter.chapter3;
+
+public class Adg {
+    public static void main(String[] args) {
+        int t = 0b1000100;
+        System.out.println(t);
+    }
+}
---
+++ src/bookchapter/chapter3/ScopeErr.java
@@ -0,0 +1,10 @@
+package bookchapter.chapter3;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {                 //creates a new scope
+            //int bar = 2; // Compile-time error -bar already defined!
+        }
+    }
+}
