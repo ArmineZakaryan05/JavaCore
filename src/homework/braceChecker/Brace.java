e6ef9f215066b063040764485526978f754c4e0b
User <zakaryanarmine991@gmail.com>
2024-09-07 21:22:09 +0400

    Homework

---
+++ src/homework/braceChecker/Brace.java
@@ -0,0 +1,11 @@
+package homework.braceChecker;
+
+public class Brace {
+    char brace;
+    int index;
+
+    Brace(char brace, int index) {
+        this.brace = brace;
+        this.index = index;
+    }
+}
---
+++ src/homework/braceChecker/BraceCheckerTest.java
@@ -0,0 +1,17 @@
+package homework.braceChecker;
+
+import java.util.Scanner;
+
+public class BraceCheckerTest {
+
+    public static void main(String[] args) {
+        // String text = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input text! ");
+        String text = scanner.nextLine();
+
+        BraceChecker bc = new BraceChecker(text);
+        bc.check();
+
+    }
+}
